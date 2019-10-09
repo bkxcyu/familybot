e1e726eeade3145ce883bfe7b1e27f9422d9dd81
bkxcyu_thinkpad <bkxcyu#gmali.com>
2019-10-09 14:06:01 +0800

    ok

---
+++ src/dji_board/src/dji_board_node.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+
+import rospy
+import serial
+import string
+import math
+import sys
+import json
+import time
+
+
+rospy.init_node("dji_board_node")
+#pub = rospy.Publisher('chassis_velocities', Imu, queue_size=1)
+
+port='/dev/dji_board'
+rospy.loginfo("Opening %s...", port)
+#控制电机速度
+def send_command(vx,vy,vz):
+    data = { "speed":[vx,vy,vz] }
+    data_string = json.dumps(data)
+    ser.write('*'+ data_string +';')
+
+
+try:
+    ser = serial.Serial(port=port, baudrate=115200, timeout=5)
+except serial.serialutil.SerialException:
+    rospy.logerr("Dji not found at port "+port + ". Did you specify the correct port in the launch file?")
+    sys.exit(0)
+
+#ser.write('t\n')
+
+
+while not rospy.is_shutdown():
+    line = ser.readline()
+    #rospy.loginfo("I have got chassis velicities: " + line)
+    #解析字符串为python 对象
+    try:
+        de_json = json.loads(line)
+        linear_x = de_json[1][0]
+        linear_y = de_json[1][1]
+        angular_z = de_json[1][2]
+        rospy.loginfo("linear_x: " + str(linear_x) + ", linear_y: " + str(linear_y)+ ", angular_z: " + str(angular_z) + "\n\n")
+    except:
+        None
+    #rospy.loginfo(de_json)
+    #控制电机速度
+    speed_x = 0
+    speed_y = 30
+    speed_z = 0
+    send_command(speed_x,speed_y,speed_z)
+#关闭节点时停止电机
+#time.sleep(1)
+send_command(0,0,0)
+ser.close
+
+#关闭节点时先按control-s再按control-c
